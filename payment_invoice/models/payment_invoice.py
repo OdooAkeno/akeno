de5fc72b116c66bf9bfddab6a5513b4e03603b97
bayux <gbayux@gmail.com>
2020-05-12 15:22:14 +0100

    [FIX] account.move to account.invoice

--- payment_invoice/models/payment_invoice.py
+++ payment_invoice/models/payment_invoice.py
@@ -4,15 +4,15 @@ from odoo.exceptions import UserError
 class PaymentInvoice(models.Model):
     _inherit = "account.payment"
 
-    invoice_id = fields.Many2one('account.move', string='Facture à recouvrer', tracking=True)
+    invoice_id = fields.Many2one('account.invoice', string='Facture à recouvrer', tracking=True)
 
     @api.onchange('partner_id')
     def _onchange_partner(self):
         if self.partner_id and self.partner_type == "customer":
-            invoice_id = self.env['account.move'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'open')]).id
+            invoice_id = self.env['account.invoice'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'open'), ('type', '=', 'out_invoice')]).id
         
         elif self.partner_id and self.partner_type == "supplier":
-            invoice_id = self.env['account.move'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'open')]).id
+            invoice_id = self.env['account.invoice'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'open'), ('type', '=', 'in_invoice')]).id
         
         else:
             invoice_id = None
