190b149e1a2b04abd6d8d6c7a1cc39e3c95cb4b6
bayux <gbayux@gmail.com>
2020-05-13 10:04:29 +0100

    Reconciliation facture - paiement

--- payment_invoice/models/payment_invoice.py
+++ payment_invoice/models/payment_invoice.py
@@ -15,3 +15,26 @@ class PaymentInvoice(models.Model):
 
     		if self.payment_type == "outbound":
     			return {'domain': {'invoice_id': [('partner_id', 'in', [self.partner_id.id]), ('type', '=', "in_invoice" ), ('state', '=', 'open')] }}
+    	else:
+    		if self.payment_type == "inbound":
+    			return {'domain': {'invoice_id': [('type', '=', "out_invoice" ), ('state', '=', 'open')] }}
+
+    		if self.payment_type == "outbound":
+    			return {'domain': {'invoice_id': [('type', '=', "in_invoice" ), ('state', '=', 'open')] }}
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+	
+	payment_id = fields.Many2one('account.payment',  string=u'Paiement effectué')
+
+	@api.multi
+    def _compute_payments_widget_to_reconcile_info(self):
+		res=super(AccountInvoice,self)._compute_payments_widget_to_reconcile_info()
+			for invoice in self:
+
+				if invoice.type = "out_invoice":
+                	domain.extend([(self.payment_id.invoice_id, '=', self.id)])
+            	if invoice.type = "in_invoice":
+                	domain.extend([(self.payment_id.invoice_id, '=', self.id)])
+		return res
