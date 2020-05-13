1705d27b636064d2195e64b48f38a4a5facb2617
bayux <gbayux@gmail.com>
2020-05-13 08:42:40 +0100

    ADD

--- payment_invoice/models/payment_invoice.py
+++ payment_invoice/models/payment_invoice.py
@@ -5,3 +5,10 @@ class PaymentInvoice(models.Model):
     _inherit = "account.payment"
 
     invoice_id = fields.Many2one('account.invoice',  string=u'Facture à recouvrer')
+
+    @api.onchange('partner_id')
+    def _onchange_invoice_id(self):
+
+    	if self.partner_id:
+    		return {'domain': {'invoice_id': [('partner_id', 'in', [self.partner_id.id])]}}
+
