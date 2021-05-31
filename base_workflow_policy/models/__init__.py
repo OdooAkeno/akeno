8d572e16da8e5c56262f3a40ae4705952547af7b
nwatsockjj <60470088+nwatsockjj@users.noreply.github.com>
2021-05-31 12:52:57 +0100

    [ADD] app base_workflow_policy

---
+++ base_workflow_policy/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    base_workflow_policy,
+    base_workflow_policy_object,
+)
